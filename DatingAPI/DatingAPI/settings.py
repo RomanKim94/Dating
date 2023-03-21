7e23885af7164a12df0a2a93c395c6c59e81f21f
Kim Roman <romankim94@gmail.com>
2023-03-21 18:13:07 +0300

    Renamed user model from "User" to "Person". Made migrations for model "Person". Override parameter "upload_to" in attribute "photo" (ImageField)

--- DatingAPI/DatingAPI/settings.py
+++ DatingAPI/DatingAPI/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    'apps.user.apps.UserConfig',
+    'apps.person.apps.PersonConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'person.Person'
---
+++ DatingAPI/apps/person/models.py
@@ -0,0 +1,27 @@
+import uuid
+from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Person(AbstractUser):
+
+    def get_file_path(self, filename):
+        username = self.username
+        date = datetime.today().strftime('%Y.%m.%d')
+        filename = uuid.uuid4[:10]+filename
+        return f'photos/{username}/{date}/{filename}'
+
+    SEX = {
+        ('M', 'Male'),
+        ('F', 'Female'),
+    }
+
+    sex = models.CharField(choices=SEX, max_length=1)
+    photo = models.ImageField(upload_to=get_file_path)
+    # Yellow mark: may return an exception. Try this and override if exception is thrown
+    USERNAME_FIELD = 'username'
+
+
+
