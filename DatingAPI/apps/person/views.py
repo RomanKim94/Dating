c08505b83df283af9291c8905967c963a4cfb5d8
Kim Roman <romankim94@gmail.com>
2023-03-31 18:50:59 +0300

    Continue allowed hosts debugging

--- DatingAPI/apps/person/views.py
+++ DatingAPI/apps/person/views.py
@@ -26,7 +26,6 @@ class PersonViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        print(os.environ.get('ALLOWED_HOSTS', 'localhost').split(','))
         if self.action in ['list', ]:
             qs = qs.exclude(username=self.request.user.username)
             qs = PersonService.add_distance(user=self.request.user, qs=qs)
